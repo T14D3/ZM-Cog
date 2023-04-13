b1c859088abdaca7b0d5ebec7da8bcdfb383fdd1
T14D3 <73843330+T14D3@users.noreply.github.com>
2023-04-13 18:31:43 +0200

    Update steamcog.py

--- Steamcog/steamcog.py
+++ Steamcog/steamcog.py
@@ -1,4 +1,5 @@
-from discord.ext import commands
+from redbot.core import commands
+from typing import Dict, Union
 
 class SteamCog(commands.Cog):
     def __init__(self, bot):
@@ -9,33 +10,27 @@ class SteamCog(commands.Cog):
         steam_id = steam_id.strip()
         if steam_id:
             with open("steam_ids.txt", "a") as f:
-                f.write(f"{ctx.author.id}-{steam_id}\n")
+                f.write(f"{ctx.author.id}:{steam_id}\n")
             await ctx.send(f"{ctx.author.mention}, your Steam ID has been saved.")
         else:
             await ctx.send(f"{ctx.author.mention}, you need to provide a Steam ID.")
 
     @commands.command(name="cleanlist")
-    @commands.has_permissions(administrator=True)
-    async def cleanlist(self, ctx, *role_ids):
-        role_ids = [int(r) for r in role_ids if r.isdigit()]
-        if not role_ids:
-            return await ctx.send("Please provide at least one valid role ID.")
-
-        lines_to_keep = []
+    async def cleanlist(self, ctx, *role_ids: Union[int]):
         with open("steam_ids.txt", "r") as f:
-            for line in f:
-                discord_id, steam_id = line.strip().split("-")
-                member = await ctx.guild.fetch_member(discord_id)
-                if not member:
-                    continue
-                member_role_ids = [r.id for r in member.roles]
-                if any(r_id in member_role_ids for r_id in role_ids):
-                    lines_to_keep.append(line)
-
+            steam_ids = f.read().strip().split("\n")
+        new_steam_ids = []
+        for steam_id in steam_ids:
+            discord_id, steam_id = steam_id.split(":")
+            member = ctx.guild.get_member(int(discord_id))
+            if member:
+                roles = [role.id for role in member.roles]
+                if any(role_id in roles for role_id in role_ids):
+                    new_steam_ids.append(f"{discord_id}:{steam_id}")
         with open("steam_ids.txt", "w") as f:
-            f.writelines(lines_to_keep)
+            f.write("\n".join(new_steam_ids))
+        await ctx.send(f"{len(steam_ids) - len(new_steam_ids)} Steam IDs have been removed.")
 
-        await ctx.send("Done. The Steam ID list has been cleaned.")
         
 def setup(bot):
     bot.add_cog(SteamCog(bot))
